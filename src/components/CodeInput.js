24fd243a6736dcb77037bed9423db728548bbfb5
sei-nah <sina.krumhard@uzh.ch>
2022-03-26 19:51:48 +0100

    CodeInput: Added input send

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {Icon, IconButton, InputAdornment, TextField, useTheme} from "@mui/material";
 import {css} from "@emotion/react";
+import {api} from "../utils/api";
 
 const CodeInput = props => {
     const theme = useTheme();
@@ -10,14 +11,41 @@ const CodeInput = props => {
           font-family: Source Code Pro, sans-serif;
         `
     };
+    const [currentInput, setCurrentInput] = useState("");
+    const [waiting, setWaiting] = useState(false);
+
+    const sendCurrentCode = async () => {
+        setWaiting(true);
+        try {
+            const result = await api.post('/command', {command: currentInput});
+            setCurrentInput("");
+        }
+        catch (error) {
+            console.error("Failed to send command to backend!", error);
+        }
+        setWaiting(false);
+    };
 
     return (
         <TextField
             fullWidth multiline
+            value={currentInput}
+            disabled={waiting}
+            onChange={e => setCurrentInput(e.target.value)}
+            onKeyUp={e => {
+                if (e.ctrlKey && e.code === 'Enter') {
+                    sendCurrentCode();
+                    e.preventDefault();
+                }
+                console.log(e.code);
+            }}
             InputProps={{
                 endAdornment:
                     <InputAdornment position="end">
-                        <IconButton>
+                        <IconButton
+                            disabled={currentInput.length < 1}
+                            onClick={() => sendCurrentCode()}
+                        >
                             <Icon>send</Icon>
                         </IconButton>
                     </InputAdornment>,
