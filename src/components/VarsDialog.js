be28a8983bd7a8f6429b1cbc4e3ca7c7a9f21bc2
Chesta <chesta@protonmail.com>
2022-05-15 16:53:56 +0200

    VarsDialog: adjust color, react to outside clicking

--- src/components/VarsDialog.js
+++ src/components/VarsDialog.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
 import {generateVarChips} from "../utils/render-utils";
+import {useTheme} from "@emotion/react";
 
 const VarsDialog = props => {
+    const theme = useTheme();
     return (
-        <Dialog open={props.open}>
+        <Dialog open={props.open} PaperProps={{sx: {background: theme.palette.background.paper}}} onClose={props.onClose}>
             <DialogTitle>Current Variables</DialogTitle>
             <DialogContent>
                 {generateVarChips(props.node ? props.node.data.vars : {})}
